da2963da92acbd8a40251a983c18bde5cfe4cbf3
arif20026 <arifislamipe026@gmail.com>
2024-01-31 01:47:05 +0600

    create post and get api for custom asset request

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,9 +14,6 @@ console.log(process.env.DB_PASS)
 app.use(cors())
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8wwrvjl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,16 +27,35 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    //   await client.connect();
+
+
+       const customRequestCollection =client.db("assetDb").collection("customRequests")
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
+
+    app.post('/customRequests', async (req, res) => {
+        const customRequest = req.body;
+        const result = await customRequestCollection.insertOne(customRequest);
+        res.send(result);
+      });
+
+
+      app.get('/customRequests', async (req, res) => {
+        const result = await customRequestCollection.find().toArray();
+        res.send(result);
+      });
+  
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 
